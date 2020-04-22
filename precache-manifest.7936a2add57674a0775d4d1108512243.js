2c11c450574cdb2463aa94f815c3e5670668bb04
Artem Shalamov <iart1756@gmail.com>
2020-04-22 19:57:40 +0500

    Updates

---
+++ precache-manifest.7936a2add57674a0775d4d1108512243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f33ce0878c33ad71aa8421e385f3e0",
+    "url": "/artemWebDevep/myPortfolioReactgit/index.html"
+  },
+  {
+    "revision": "e30cf126b169d60f08fd",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "a3c2a203cb4b08c9e70b",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/css/main.6810d3c6.chunk.css"
+  },
+  {
+    "revision": "e30cf126b169d60f08fd",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/js/2.56fce931.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/js/2.56fce931.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3c2a203cb4b08c9e70b",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/js/main.7f153d9a.chunk.js"
+  },
+  {
+    "revision": "1e62e50efe79c9390e15",
+    "url": "/artemWebDevep/myPortfolioReactgit/static/js/runtime-main.db954c37.js"
+  }
+]);
